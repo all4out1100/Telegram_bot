72603dc529c790268c37225c226a9dc8c286de70
all4out1100 <all4out11.0@gmail.com>
2024-09-05 23:54:39 +0530

    Update tmate.sh

--- tmate.sh
+++ tmate.sh
@@ -12,9 +12,19 @@ tar -xf tmate.tar.xz
 echo "Making tmate executable..."
 chmod +x tmate-2.4.0-static-linux-amd64/tmate
 
-# Run tmate
-echo "Running tmate..."
-./tmate-2.4.0-static-linux-amd64/tmate
+# Run tmate in the background
+echo "Running tmate in the background..."
+./tmate-2.4.0-static-linux-amd64/tmate -S /tmp/tmate.sock new-session -d
+
+# Wait for tmate to start
+echo "Waiting for tmate to start..."
+sleep 5
+
+# Display the SSH and Web session information
+echo "Fetching SSH and Web connection info..."
+./tmate-2.4.0-static-linux-amd64/tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+./tmate-2.4.0-static-linux-amd64/tmate -S /tmp/tmate.sock display -p '#{tmate_ssh_ro}'
+./tmate-2.4.0-static-linux-amd64/tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
 
 # Keep the session alive for 24 hours (86400 seconds)
 echo "Keeping session alive for 24 hours..."
